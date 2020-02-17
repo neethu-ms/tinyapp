c9ef631458a584f5d681dfb338b4b45ccb26fac5
neethu-ms <msneethu2989@gmail.com>
2020-02-17 21:46:42 +0000

    Added ejs directory structure

--- express_server.js
+++ express_server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set('view-engine', 'ejs');
+app.set('view engine', 'ejs');
 const urlDatabase = {
   'b2xVn2': "http://www.lighthouselabs.ca",
   '9sm5xK': "http://www.google.com"
@@ -22,4 +22,9 @@ app.get('/urls.json', (req, res) => {
 
 app.get('/hello', (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase};
+  res.render("urls_index", templateVars);
 });
